30b5a713bda3dfb2f4b0d5107c5a74a10441f5a4
Abdelwahab Ahmed Shandy <119771784+abdelwahab-ahmed-shandy@users.noreply.github.com>
2025-06-11 10:54:50 +0300

    update readme.mk

--- BLL/GloubalUsing/GloubalUsing.cs
+++ BLL/GloubalUsing/GloubalUsing.cs
@@ -10,6 +10,7 @@ global using Microsoft.EntityFrameworkCore;
 
 global using Models;
 global using MovieMart;
+global using DAL.Enums;
 global using DAL.Models;
 global using BLL.Utilities;
 global using MovieMart.Models;
@@ -22,4 +23,7 @@ global using DAL.ViewModels.Category;
 global using BLL.Services.Interfaces;
 global using BLL.Services.Implementations;
 global using DAL.Repositories.IRepositories;
+global using System.ComponentModel.DataAnnotations;
+
+
 
